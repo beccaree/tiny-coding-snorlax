6a184de7a0e642cf5eb8b424918f8ee9d1b00609
unknown <kris.t.tsoi@gmail.com>
2016-08-01 14:16:38 +1200

    and stefan intial complete calculate method in bottomlevelcalulator

--- se306Project1/src/project1/BottomLevelCalculator.java
+++ se306Project1/src/project1/BottomLevelCalculator.java
@@ -1,13 +1,13 @@
 package project1;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Set;
 
 import org.jgrapht.*;
 import org.jgrapht.graph.DefaultDirectedGraph;
-import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultWeightedEdge;
+
+import com.sun.javafx.geom.Edge;
 
 /**
  * Class is used to calculate the weight of the bottom levels
@@ -18,19 +18,44 @@ public class BottomLevelCalculator {
 	
 	//hashmap to store all the weights as graph can not store weights
 
-	private void calculate (){
+	private void calculate(){
 		
-		DirectedGraph<String, DefaultWeightedEdge> directedGraph =
-	            new DefaultDirectedGraph<String,  DefaultWeightedEdge>
+		DirectedGraph<Vertex, DefaultWeightedEdge> directedGraph =
+	            new DefaultDirectedGraph<Vertex,  DefaultWeightedEdge>
 	            ( DefaultWeightedEdge.class);
 	
-		Queue queue = new LinkedList<>();
+		Queue<Vertex> queue = new LinkedList<>();
 		
-		for ( String node: directedGraph.vertexSet()){
+		//Add all leafs to the queue
+		for ( Vertex node: directedGraph.vertexSet()){
 			if(directedGraph.outDegreeOf(node)==0){
 				queue.add(node);
 				//TODO create something that can store the bottom level weight
 			}
+		}
+		
+		//while the queue is not empty calculate the weight
+		while (!queue.isEmpty()){
+			//get the head node from the queue
+			Vertex node = queue.remove();
+			for (DefaultWeightedEdge e: directedGraph.incomingEdgesOf(node)){
+				//get the destination node of the edge
+//				Vertex destinationNode = directedGraph.getEdgeTarget(e);
+				
+				
+				//get node from which edge comes from
+				Vertex sourceNode =directedGraph.getEdgeSource(e);
+				sourceNode.SetBottomLevel(Math.max(sourceNode.getBottomLevel(),(node.getBottomLevel()+node.getWeight())));
+				
+				//remove used edge
+				directedGraph.removeEdge(node, sourceNode);
+				
+				//if the source node is now a leaf then add it into the queue
+				if(directedGraph.outDegreeOf(sourceNode)==0){
+					queue.add(sourceNode);
+				}
+			}
+			
 		}
 		
 	}
--- se306Project1/src/project1/Vertex.java
+++ se306Project1/src/project1/Vertex.java
@@ -2,7 +2,43 @@ package project1;
 
 public class Vertex {
 
-	//name weight 
+	//variables
 	private String name;
-	private int bottomLevelWeight;
+	private int weight;
+	private int bottomLevel;
+	
+	//Get methods
+	/**
+	 * Get Name of Vertex
+	 * @return String name
+	 */
+	public String getName(){
+		return name;
+	}
+	
+	/**
+	 * Get weight of vertex
+	 * @return int weight
+	 */
+	public int getWeight() {
+		return weight;
+	}
+	
+	/**
+	 * Get bottom level (critical path from one node)
+	 * @return int critical path from one node
+	 */
+	public int getBottomLevel() {
+		return bottomLevel;
+	}
+	
+	//Set methods
+	/**
+	 * Set new value to bottom level variable
+	 * @param value from bottomLevel
+	 */
+	public void SetBottomLevel(int value) {
+		bottomLevel = value;
+	}
+	
 }
