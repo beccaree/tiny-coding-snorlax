0f47a7e8985a8beed2a5049ead49512499a4383d
unknown <kris.t.tsoi@gmail.com>
2016-08-01 13:21:40 +1200

    and stefan - calcuate method and create vertex class

--- se306Project1/src/project1/BottomLevelCalculator.java
+++ se306Project1/src/project1/BottomLevelCalculator.java
@@ -1,4 +1,13 @@
 package project1;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.jgrapht.*;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultWeightedEdge;
 
 /**
  * Class is used to calculate the weight of the bottom levels
@@ -6,5 +15,26 @@ package project1;
  *
  */
 public class BottomLevelCalculator {
+	
+	//hashmap to store all the weights as graph can not store weights
 
+	private void calculate (){
+		
+		DirectedGraph<String, DefaultWeightedEdge> directedGraph =
+	            new DefaultDirectedGraph<String,  DefaultWeightedEdge>
+	            ( DefaultWeightedEdge.class);
+	
+		Queue queue = new LinkedList<>();
+		
+		for ( String node: directedGraph.vertexSet()){
+			if(directedGraph.outDegreeOf(node)==0){
+				queue.add(node);
+				//TODO create something that can store the bottom level weight
+			}
+		}
+		
+	}
+	
+	
+	
 }
---
+++ se306Project1/src/project1/Vertex.java
@@ -0,0 +1,8 @@
+package project1;
+
+public class Vertex {
+
+	//name weight 
+	private String name;
+	private int bottomLevelWeight;
+}
