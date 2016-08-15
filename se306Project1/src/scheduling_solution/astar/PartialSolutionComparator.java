aeb81aa958fa027a73dc46bec739e7f1f6cc98e0
kris-t-tsoi <kris.t.tsoi@gmail.com>
2016-08-15 21:09:55 +1200

    add comments to ParticalSolutionComparator

--- se306Project1/src/scheduling_solution/astar/PartialSolutionComparator.java
+++ se306Project1/src/scheduling_solution/astar/PartialSolutionComparator.java
@@ -5,6 +5,12 @@ import java.util.Comparator;
 public class PartialSolutionComparator implements Comparator<PartialSolution> {
 
 	@Override
+	/**
+	 * Compares two PartialSolutions to determine which solution will have a quicker estimated runtime
+	 * returns 	1 - Second partialSolution is quicker
+	 * 		   -1 - First partialSolution is quicker
+	 * 			0 - Both  partialSolution are equal
+	 */
 	public int compare(PartialSolution p1, PartialSolution p2) {
 		int t1 = p1.getMinimumTime();
 		int t2 = p2.getMinimumTime();
