0548074630dba94f40e7df599b67d9e8c7c85315
Kristy Tsoi <kris.t.tsoi@gmail.com>
2016-08-25 10:00:46 +1200

    Add comments into Branch and Bound related classes
    BranchAndBound and PartialSolution classes

--- se306Project1/src/scheduling_solution/branch_and_bound/PartialSolution.java
+++ se306Project1/src/scheduling_solution/branch_and_bound/PartialSolution.java
@@ -7,18 +7,39 @@ import org.jgrapht.graph.DefaultWeightedEdge;
 import scheduling_solution.graph.GraphInterface;
 import scheduling_solution.graph.Vertex;
 
+/**
+ * Partial solution class used by the Branch and Bound Algorithm
+ */
 public class PartialSolution extends scheduling_solution.astar.PartialSolution {
 	
+	/**
+	 * Initialise the first partial solution using a starting vertex
+	 * @param graph - digraph of tasks and their dependencies
+	 * @param numProcessors - number of processors 
+	 * @param v - starting vertex (has not parent vertices)
+	 * @param processorNumber - processor to allocate vertex to
+	 */
 	public PartialSolution(GraphInterface<Vertex, DefaultWeightedEdge> graph, byte numProcessors, Vertex v,
 			byte processorNumber) {
 		super(graph, numProcessors, v, processorNumber);
 	}
 
+	/**
+	 * Adds the given vertex into an existing partial solution
+	 * @param graph - digraph of tasks and their dependencies
+	 * @param partialSolution - partial solution to add vertex to
+	 * @param v - vertex to be added in
+	 * @param processorNumber- processor to allocate vertex to
+	 */
 	public PartialSolution(GraphInterface<Vertex, DefaultWeightedEdge> graph,
 			scheduling_solution.astar.PartialSolution partialSolution, Vertex v, byte processorNumber) {
 		super(graph, partialSolution, v, processorNumber);
 	}
 	
+	/**
+	 * Use heuristics to calculate the partial solution's estimated finish time
+	 * Results are used to order solutions in the priority queue 
+	 */
 	public void calculateMinimumFinishTime(PartialSolution p, Vertex v) {
 		int maxHeuristic = Math.max(p.getMinimumFinishTime(), getAllocatedVertices().get(v).getStartTime() + v.getBottomLevel());
 		maxHeuristic = Math.max(maxHeuristic, ((BranchAndBound.getSequentialTime() + getTotalIdleTime() / numProcessors)));
